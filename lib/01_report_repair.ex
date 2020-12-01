6b03261f05ad515e84f122271bd2eed7749d758c
Bart van Nierop <mail@bvnierop.nl>
2020-12-01 16:35:31 +0100

    Finish Day 1

--- lib/01_report_repair.ex
+++ lib/01_report_repair.ex
@@ -7,6 +7,25 @@ defmodule AdventOfCode.Day01ReportRepair do
   Run it
   """
   def solve do
-    IO.puts "This is where we are solving day 1"
+    solve_a()
+    solve_b()
+  end
+
+  def solve_a do
+    numbers = input() |> Enum.map(&String.to_integer/1)
+    pairs = for x <- numbers, y <- numbers, x + y == 2020, do: x * y
+    IO.inspect pairs
+  end
+
+  def solve_b do
+    numbers = input() |> Enum.map(&String.to_integer/1)
+    pairs = for x <- numbers, y <- numbers, z <- numbers, x + y + z == 2020, do: x * y * z
+    IO.inspect pairs
+  end
+
+  def input do
+    # ["1721", "979", "366", "299", "675", "1456"]
+    File.stream!("input/01_report_repair.in")
+    |> Stream.map(&String.trim/1)
   end
 end
