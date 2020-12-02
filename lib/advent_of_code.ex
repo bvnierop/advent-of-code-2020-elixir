84c0866df92705d4ec58aba400f954469d754353
Bart van Nierop <mail@bvnierop.nl>
2020-12-02 15:20:48 +0100

    Remove `time` from using macro
    There's no point in generating it, we can just import it.

--- lib/advent_of_code.ex
+++ lib/advent_of_code.ex
@@ -6,29 +6,26 @@ defmodule AdventOfCode do
   @doc """
   Define some base functions that we want in all day solutions
   """
-  defmacro __using__(_options) do
+  defmacro __using__(_) do
     quote do
       import AdventOfCode
 
-      @doc """
-      Time a function and print its result"
-      """
-      def time(f, label) do
-        case :timer.tc f do
-          {time, result} ->
-            IO.inspect result, label: label
-            IO.puts "Runtime: #{:erlang.float_to_binary(time / 1_000, [decimals: 2])} ms."
-        end
-      end
-
-      @doc """
-      Run it
-      """
       def solve do
         time &solve_a/0, "Part 1"
         IO.puts ""
         time &solve_b/0, "Part 2"
       end
+    end
+  end
+
+  @doc """
+  Time a function and print its result"
+  """
+  def time(f, label) do
+    case :timer.tc f do
+      {time, result} ->
+        IO.inspect result, label: label
+        IO.puts "Runtime: #{:erlang.float_to_binary(time / 1_000, [decimals: 2])} ms."
     end
   end
 
