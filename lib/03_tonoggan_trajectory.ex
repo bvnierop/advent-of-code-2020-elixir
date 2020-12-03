8c4750ba3d3c8858c7ca06df3927fc40ee3d7b45
Bart van Nierop <mail@bvnierop.nl>
2020-12-03 20:23:34 +0100

    Make day 3 a lot faster

--- lib/03_tonoggan_trajectory.ex
+++ lib/03_tonoggan_trajectory.ex
@@ -2,23 +2,25 @@ defmodule AdventOfCode.Day03TobogganTrajectory do
   use AdventOfCode
 
   def tree?(x, y, map) do
-    width = Enum.at(map, 0) |> String.length
+    width = elem(map, 0) |> String.length
     adjusted_x = rem(x, width)
-    String.at(Enum.at(map, y), adjusted_x) == "#"
+    String.at(elem(map, y), adjusted_x) == "#"
   end
 
   def solve_for_slope(dx, dy, input) do
-    height = Enum.count(input)
+    height = tuple_size(input)
     Stream.iterate(0, &(&1 + 1))
     |> Enum.take_while(fn step -> step * dy < height end)
     |> Enum.count(fn step -> tree?(step * dx, step * dy, input) end)
   end
 
   def solve_a(input) do
-    solve_for_slope(3, 1, input)
+    tuple = Enum.to_list(input) |> List.to_tuple
+    solve_for_slope(3, 1, tuple)
   end
 
   def solve_b(input) do
+    tuple = Enum.to_list(input) |> List.to_tuple
     slopes = [
       {1, 1},
       {3, 1},
@@ -27,7 +29,7 @@ defmodule AdventOfCode.Day03TobogganTrajectory do
       {1, 2}
     ]
     slopes
-    |> Enum.map(fn {dx, dy} -> solve_for_slope(dx, dy, input) end)
+    |> Enum.map(fn {dx, dy} -> solve_for_slope(dx, dy, tuple) end)
     |> Enum.reduce(&*/2)
   end
 end
