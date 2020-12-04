7daab3d5986feafaba76cb497b1736ab8144104d
Bart van Nierop <mail@bvnierop.nl>
2020-12-04 19:33:03 +0100

    Learn about Stream.take_every

--- lib/03_toboggan_trajectory.ex
+++ lib/03_toboggan_trajectory.ex
@@ -1,26 +1,20 @@
 defmodule AdventOfCode.Day03TobogganTrajectory do
   use AdventOfCode
 
-  defp solve_a(input), do: solve_for_slope(3, 1, process(input))
-  defp process(input), do: Enum.to_list(input) |> List.to_tuple
+  defp solve_a(input), do: solve_for_slope(3, 1, input)
 
   defp solve_for_slope(dx, dy, map) do
-    height = tuple_size(map)
-    Stream.iterate(0, &(&1 + 1))
-    |> Enum.take_while(fn step -> step * dy < height end)
-    |> Enum.count(fn step -> tree?(step * dx, step * dy, map) end)
+    map
+    |> Stream.take_every(dy)
+    |> Stream.with_index
+    |> Enum.count(fn {row, index} -> tree?(row, index * dx) end)
   end
 
-  defp tree?(x, y, map) do
-    width = elem(map, 0) |> String.length
-    adjusted_x = rem(x, width)
-    String.at(elem(map, y), adjusted_x) == "#"
-  end
+  defp tree?(row, x), do: String.at(row, rem(x, String.length(row))) == "#"
 
   defp solve_b(input) do
-    processed = process(input)
     _slopes = [{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}]
-    |> Enum.map(fn {dx, dy} -> solve_for_slope(dx, dy, processed) end)
+    |> Enum.map(fn {dx, dy} -> solve_for_slope(dx, dy, input) end)
     |> Enum.reduce(&*/2)
   end
 end
