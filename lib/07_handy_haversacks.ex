abaed5536a72176b53fc04cbcecac29b54d923e6
Bart van Nierop <mail@bvnierop.nl>
2020-12-07 21:07:37 +0100

    Reverse default order of graph

--- lib/07_handy_haversacks.ex
+++ lib/07_handy_haversacks.ex
@@ -2,40 +2,40 @@ defmodule AdventOfCode.Day07HandyHaversacks do
   use AdventOfCode
 
   def solve_a(input) do
-    input |> parse |> find_bags_containing("shiny gold") |> MapSet.size
+    input
+    |> parse
+    |> invert
+    |> find_bags_containing("shiny gold")
+    |> MapSet.size
   end
 
   def solve_b(input) do
     input
     |> parse
-    |> invert
     |> count_required_bags("shiny gold")
     |> (&(&1 - 1)).()
   end
 
+  def parse(lines) when is_list(lines) do
+    Enum.map(lines, &parse/1)
+    |> Map.new
+  end
+
   def parse(line) when is_binary(line) do
-    if Regex.match?(~r/^(.+?) bags contain no other bags.$/, line) do
-      {:none}
-    else
-      [bag] = Regex.run(~r/^(.+?) bags contain/, line, capture: :all_but_first)
-      {:some,
-       line
-       |> String.replace(bag <> " bags contain ", "")
-       |> String.split(",")
-       |> Enum.map(fn part -> Regex.run(~r/(\d+) (.+?) bags?/, part, capture: :all_but_first) end)
-       |> Enum.map(fn [amount, contains] -> {contains, %{bag => String.to_integer(amount)}} end)}
+    case Regex.run(~r/^(.+?) bags contain no other bags.$/, line, capture: :all_but_first) do
+      [bag] -> {bag, %{}}
+      nil -> parse_complex_line(line)
     end
   end
 
-  def parse(lines) do
-    Enum.reduce(lines, %{}, fn line, acc ->
-      case parse(line) do
-        {:none} -> acc
-        {:some, bags} ->
-          Enum.reduce(bags, acc, fn {bag, contained_by}, acc ->
-            Map.put(acc, bag, Map.merge(acc[bag] || %{}, contained_by)) end)
-      end
-    end)
+  def parse_complex_line(line) do
+    [bag] = Regex.run(~r/^(.+?) bags contain/, line, capture: :all_but_first)
+    {bag, 
+     line
+     |> String.replace(bag <> " bags contain ", "")
+     |> String.split(",")
+     |> Enum.map(fn part -> Regex.run(~r/(\d+) (.+?) bags?/, part, capture: :all_but_first) end)
+     |> Enum.reduce(%{}, fn [count, bag], bags -> Map.put(bags, bag, String.to_integer(count)) end)}
   end
 
   def find_bags_containing(graph, bag) do
@@ -50,8 +50,8 @@ defmodule AdventOfCode.Day07HandyHaversacks do
       Enum.reduce(contained_by, inverted_graph, fn {containing_bag, count}, inverted_graph ->
         Map.put(inverted_graph, containing_bag,
           Map.put(inverted_graph[containing_bag] || %{}, bag, count))
-        end)
       end)
+    end)
   end
 
   def count_required_bags(graph, bag) do
--- test/07_handy_haversacks_test.exs
+++ test/07_handy_haversacks_test.exs
@@ -7,13 +7,8 @@ defmodule AdventOfCode.Day07HandyHaversacksTest do
 
   @lines [
     "light red bags contain 1 bright white bag, 2 muted yellow bags.",
-    # "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
     "bright white bags contain 1 shiny gold bag.",
     "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
-    # "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
-    # "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
-    # "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
-    # "faded blue bags contain no other bags.",
     "dotted black bags contain no other bags."
   ]
 
@@ -29,26 +24,28 @@ defmodule AdventOfCode.Day07HandyHaversacksTest do
     "dotted black bags contain no other bags."
   ]
 
-  @graph %{"shiny gold" => %{"bright white" => 1, "muted yellow" => 2},
-           "faded blue" => %{"muted yellow" => 9},
-           "muted yellow" => %{"light red" => 2},
-           "bright white" => %{"light red" => 1}}
+  @graph %{"bright white" => %{"shiny gold" => 1},
+           "light red" => %{"bright white" => 1, "muted yellow" => 2},
+           "muted yellow" => %{"faded blue" => 9, "shiny gold" => 2},
+           "dotted black" => %{}}
+
 
   test "parses input" do
     assert parse(@lines) == @graph
   end
 
   test "finds out which bags can (indirectly) contain a another bag" do
-    assert find_bags_containing(@graph, "shiny gold") == MapSet.new(["bright white", "muted yellow", "light red"])
+    assert find_bags_containing(invert(@graph), "shiny gold") == MapSet.new(["bright white", "muted yellow", "light red"])
   end
 
   test "invert graph" do
-    assert invert(@graph) == %{"light red" => %{"bright white" => 1, "muted yellow" => 2},
-                               "bright white" => %{"shiny gold" => 1},
-                               "muted yellow" => %{"shiny gold" => 2, "faded blue" => 9}}
+    assert invert(@graph) == %{"bright white" => %{"light red" => 1},
+                               "muted yellow" => %{"light red" => 2},
+                               "faded blue" => %{"muted yellow" => 9},
+                               "shiny gold" => %{"bright white" => 1, "muted yellow" => 2}}
   end
 
   test "count bags" do
-    assert count_required_bags(invert(parse(@all_lines)), "shiny gold") == 33
+    assert count_required_bags(parse(@all_lines), "shiny gold") == 33
   end
 end
