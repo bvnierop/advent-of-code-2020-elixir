60e162b67df749eebbcd6f3359521c077bd74c50
Bart van Nierop <mail@bvnierop.nl>
2020-12-14 13:14:24 +0100

    Solve Day 13, Part 2

--- lib/13_shuttle_search.ex
+++ lib/13_shuttle_search.ex
@@ -12,7 +12,19 @@ defmodule AdventOfCode.Day13ShuttleSearch do
   end
 
   def solve_b(input) do
-    Enum.at(input, 0)
+    [_, bus_line] = input
+
+    busses = bus_line
+    |> String.replace("x", "1")
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+
+    last_departure_at = busses
+    |> Enum.reverse
+    |> Enum.with_index
+    |> crt
+
+    last_departure_at - Enum.count(busses) + 1
   end
 
   def parse([arrival, busses]) do
@@ -29,5 +41,28 @@ defmodule AdventOfCode.Day13ShuttleSearch do
   def wait_time(arrival, bus_interval) do
     minutes_too_early = rem(arrival, bus_interval)
     if minutes_too_early == 0, do: 0, else: bus_interval - minutes_too_early
+  end
+
+  # Chinese Remainder Theorem shamelessly stolen from Rosetta Code (F# implementation)
+  def crt(seq) do
+    fN = Enum.reduce(seq, 1, fn {n, _a}, acc -> n * acc end)
+    part = Enum.reduce(seq, 0, fn {n, a}, res ->
+      res + a * div(fN, n) * mi(n, Integer.mod(div(fN, n), n))
+    end)
+    Integer.mod(part, fN)
+  end
+
+  # Modular Inverse shamelessly stolen from Rosetta Code (F# implementation)
+  def mi(n, g) do
+    Integer.mod(n + mi_rec(n, 1, 0, g, 0, 1), n)
+  end
+
+  defp mi_rec(n, i, g, e, l, a) do
+    case e do
+      0 -> g
+      _ ->
+        o = div(n, e)
+        mi_rec(e, l, a, (n - o * e), (i - o * l), (g - o * a))
+    end
   end
 end
--- test/13_shuttle_search_test.exs
+++ test/13_shuttle_search_test.exs
@@ -7,4 +7,19 @@ defmodule AdventOfCode.Day13ShuttleSearchTest do
     assert wait_time(10, 3) == 2
     assert wait_time(10, 2) == 0
   end
+
+  test "chinese remainder theorem" do
+    assert crt([{3, 2}, {5, 3}, {7, 2}]) == 23
+    assert crt([{13, 0}, {7, 1}]) == 78
+    assert crt([{19, 0}, {31, 1}, {1, 2}, {59, 3}, {13, 6}, {7, 7}]) == 1068788
+  end
+
+  test "modular inverse" do
+    assert mi(2017, 42) == 1969
+  end
+
+  test "b" do
+    assert solve_b(["", "7,13,x,x,59,x,31,19"]) == 1068781
+    assert solve_b(["", "67,x,7,59,61"]) == 779210
+  end
 end
