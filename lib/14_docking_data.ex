a43971f79815e03a65cab87c4fa27299ec6bf5a5
Bart van Nierop <mail@bvnierop.nl>
2020-12-14 18:43:41 +0100

    Solve Day 14, Part 2

--- lib/14_docking_data.ex
+++ lib/14_docking_data.ex
@@ -13,18 +13,69 @@ defmodule AdventOfCode.Day14DockingData do
     end
 
     def apply_mask(num, {zeroes, ones}), do: num &&& zeroes ||| ones
+
+    def reduce_b(instructions) do
+      {_mask, mem} = Enum.reduce(instructions, {"", %{}}, fn
+        {:mask, mask}, {_mask, mem} -> {mask, mem}
+        {:mem, dst, val}, {str_mask, mem} ->
+          masked_dst = apply_mask2(dst, str_mask)
+          new_mem = stream(masked_dst)
+          |> Enum.reduce(mem, fn dst, mem ->
+            Map.put(mem, dst, val)
+          end)
+          {str_mask, new_mem}
+      end)
+      mem
+    end
+
+    def apply_mask2(num, mask) do
+      num_str = Integer.to_string(num, 2)
+      |> String.pad_leading(String.length(mask), "0")
+
+      for n <- 0..(String.length(mask) - 1),
+        v = String.at(num_str, n),
+        m = String.at(mask, n),
+        into: "",
+        do: (if m == "0", do: v, else: m)
+    end
+
+    def next(mask), do: String.replace(mask, "X", "0") |> String.to_integer(2)
+    def next(mask, cur), do: next(mask, String.length(mask) - 1, cur)
+    def next("X"<>_rest, 0, 0), do: 1
+    def next("X"<>_rest, 0, 1), do: nil
+    def next(_mask, 0, _), do: nil
+    def next(mask, n, cur) do
+      case String.at(mask, n) do
+        "X" -> case cur &&& 1 do
+                 0 -> cur ||| 1
+                 1 -> case next(mask, n - 1, cur >>> 1) do
+                        nil -> nil
+                        n -> n <<< 1
+                      end
+               end
+        _ -> case next(mask, n - 1, cur >>> 1) do
+               nil -> nil
+               n -> (n <<< 1) ||| (cur &&& 1)
+             end
+      end
+    end
+
+    def stream(mask) do
+      Stream.unfold({mask, next(mask)}, fn
+        {_, nil} -> nil
+        {mask, cur}  -> {cur, {mask, next(mask, cur)}}
+      end)
+    end
   end
 
   defmodule Parser do
-    def parse(input) when is_list(input) do
-      Enum.map(input, &parse/1)
-    end
-
-    def parse(input) when is_binary(input) do
+    def parse(input, must_parse_mask \\ true)
+    def parse(input, must_parse_mask) when is_list(input), do: Enum.map(input, &parse(&1, must_parse_mask))
+    def parse(input, must_parse_mask) when is_binary(input) do
       case input do
         "mask" <> _ ->
           [mask] = Regex.run(~r/mask = (.+)/, input, capture: :all_but_first)
-          parse_mask(mask)
+          if must_parse_mask, do: parse_mask(mask), else: {:mask, mask}
         "mem" <> _ ->
           [dst, val] = Regex.run(~r/mem\[(\d+)\] = (\d+)/, input, capture: :all_but_first) 
           {:mem, String.to_integer(dst), String.to_integer(val)}
@@ -49,6 +100,10 @@ defmodule AdventOfCode.Day14DockingData do
   end
 
   def solve_b(input) do
-    Enum.at(input, 0)
+    input
+    |> Parser.parse(false)
+    |> Computer.reduce_b
+    |> Map.values
+    |> Enum.sum
   end
 end
--- test/14_docking_data_test.exs
+++ test/14_docking_data_test.exs
@@ -33,4 +33,30 @@ defmodule AdventOfCode.Day14DockingDataTest do
     instructions = Parser.parse(@test_input)
     assert Computer.reduce(instructions) == %{7 => 101, 8 => 64}
   end
+
+  test "build stream" do
+    assert Computer.next("XXX") == 0
+    assert Computer.next("XXX", 0) == 1
+    assert Computer.next("XXX", 1) == 2
+    assert Computer.next("XXX", 7) == nil
+
+    assert Computer.next("XX1") == 1
+
+    assert Computer.next("X0X", 1) == 0b100
+
+    # "X1X"
+    # 010
+    # 011
+    # 110
+    # 111
+    assert Computer.next("X1X", 0b11) == 0b110
+
+    assert Enum.to_list(Computer.stream("X1X")) == [0b010, 0b011, 0b110, 0b111]
+    assert Enum.to_list(Computer.stream("X0X")) == [0b000, 0b001, 0b100, 0b101]
+    assert Enum.to_list(Computer.stream("0X0X")) == [0b000, 0b001, 0b100, 0b101]
+  end
+
+  test "apply mask part 2" do
+    assert Computer.apply_mask2(0b101010, "00XX1100") == "00XX1110"
+  end
 end
